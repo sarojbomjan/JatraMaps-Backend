7fdbc5c91c3b29a22865feedfe543785d0e46edb
sahiramaharjan <sahiramaharjan11@gmail.com>
2025-04-04 11:08:28 +0545

    Updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { userRouter } = require("./routes/userRoutes");
 const { eventRouter } = require("./routes/eventsRoutes");
+const commentRouter = require("./routes/commentRoutes")
 
 dotenv.config();
 console.log("JWT_SECRET_KEY:", process.env.JWT_SECRET_KEY); 
@@ -16,7 +17,7 @@ app.use(express.json());
 // mount user routes
 app.use("/", userRouter);
 app.use("/comments", commentRouter)
-app.use("/moderation", userRouterMod)
+// app.use("/moderation", userRouterMod)
 app.use("/events", eventRouter);
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
