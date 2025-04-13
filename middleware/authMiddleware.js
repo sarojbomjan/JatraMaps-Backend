291c8edef57e3235b6c3dc27db7aa26190cac496
sarojbomjan <sarojbomjan7@gmail.com>
2025-04-13 07:23:00 +0545

    CleanUp Code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -14,21 +14,17 @@ const authMiddleware = async ( req, res, next) => {
         // verify token
         const decoded = jwt.verify(token, JWT_SECRET);
 
-        // find user and attach to request
         const user = await UserModel.findById(decoded.userId).select('-password');
     
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-
-        //  Attach user to request object
         req.user = user;
         req.token = token;
         next();
 
     }   catch (error) {
         console.error('Authentication error:', error.message);
-          // Specific error responses
         if (error.name === 'TokenExpiredError') {
             return res.status(401).json({
                 success: false,
