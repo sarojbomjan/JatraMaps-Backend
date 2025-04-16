c8697c1b883638fc7a48878632161f4a33fc6d83
sarojbomjan <sarojbomjan7@gmail.com>
2025-04-16 12:07:48 +0545

    Update Event Added

--- controller/eventController.js
+++ controller/eventController.js
@@ -192,6 +192,29 @@ const deleteEvent = async (req, res) => {
   }
 };
 
+// Approve event (
+const approveEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const event = await Event.findById(id);
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
+    }
+
+    event.status = "active";
+
+    const updatedEvent = await event.save();
+    res.status(200).json({
+      message: "Event approved successfully",
+      event: updatedEvent,
+    });
+  } catch (error) {
+    console.error("Error approving event: ", error);
+    res.status(500).json({ error: "Failed to approve event" });
+  }
+};
+
 module.exports = {
   createEvent,
   getUpcomingEvents,
@@ -200,4 +223,5 @@ module.exports = {
   getEventById,
   updateEvent,
   deleteEvent,
+  approveEvent,
 };
